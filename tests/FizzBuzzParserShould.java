c4d0d44566bcc586540d88e96d673d756bbf3e54
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2015-07-25 23:49:02 +0100

    Removed ToDo list

--- tests/FizzBuzzParserShould.java
+++ tests/FizzBuzzParserShould.java
@@ -5,17 +5,6 @@ import static org.hamcrest.Matchers.is;
 
 public class FizzBuzzParserShould {
 
-    /**
-     * TODO:
-     * A number divisible by 2 has to parse to Fizz
-     * A number divisible by 3 has to parse to Buzz
-     * A number divisible by 2 and 3 has to parse to FizzBuzz
-     *
-     * Examples:
-     *
-     * 6 -> 1\nFizz\nBuzz\nFizz\n5\nFizzBuzz
-     */
-
     @Test
     public void not_parse_the_input_number() {
         assertThat(FizzBuzzParser.parse(1), is("1"));
