975ef0406a55977f1316b848ff0af0e2dbf086af
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2015-07-25 23:45:32 +0100

    Refactor: extract method and generalize

--- src/FizzBuzzParser.java
+++ src/FizzBuzzParser.java
@@ -1,9 +1,18 @@
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class FizzBuzzParser {
 
     public static String parse(int input) {
-        if (input % 2 == 0) return "1\nFizz";
-        if (input % 3 == 0) return "1\nFizz\nBuzz";
-        return "1";
+        return IntStream.rangeClosed(1, input)
+                        .mapToObj(FizzBuzzParser::parseNumber)
+                        .collect(Collectors.joining("\n"));
+    }
+
+    private static String parseNumber(int number) {
+        if (number % 2 == 0) return "Fizz";
+        if (number % 3 == 0) return "Buzz";
+        return String.valueOf(number);
     }
 
 }
