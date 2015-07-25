26bfddc342ae9d314866c93c3667b3359bfd241e
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2015-07-25 23:58:03 +0100

    Refactor: General

--- src/FizzBuzzParser.java
+++ src/FizzBuzzParser.java
@@ -1,19 +1,29 @@
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import static java.lang.String.valueOf;
+import static java.util.stream.Collectors.joining;
+
 public class FizzBuzzParser {
+
+    private static final String EMPTY = "";
 
     public static String parse(int input) {
         return IntStream.rangeClosed(1, input)
                         .mapToObj(FizzBuzzParser::parseNumber)
-                        .collect(Collectors.joining("\n"));
+                        .collect(joining("\n"));
     }
 
     private static String parseNumber(int number) {
-        if (number % 2 == 0 && number % 3 == 0) return "FizzBuzz";
-        if (number % 2 == 0) return "Fizz";
-        if (number % 3 == 0) return "Buzz";
-        return String.valueOf(number);
+        String parsedNumber = parseIsDivisibleByTwo(number).concat(parseIsDivisibleByThree(number));
+        return parsedNumber.equals(EMPTY) ? valueOf(number) : parsedNumber;
+    }
+
+    private static String parseIsDivisibleByTwo(int number) {
+        return number % 2 == 0 ? "Fizz" : EMPTY;
+    }
+
+    private static String parseIsDivisibleByThree(int number) {
+        return number % 3 == 0 ? "Buzz" : EMPTY;
     }
 
 }
