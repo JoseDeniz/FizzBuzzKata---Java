c7efe4f491146b7ecd7fcd91cf69cdb1c0de6c5f
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2015-07-25 23:08:54 +0100

    Create ToDo list

---
+++ tests/FizzBuzzParserShould.java
@@ -0,0 +1,17 @@
+public class FizzBuzzParserShould {
+
+    /**
+     * TODO:
+     * A number divisible by 2 has to parse to Fizz
+     * A number divisible by 3 has to parse to Buzz
+     * A number divisible by 2 and 3 has to parse to FizzBuzz
+     *
+     * Examples:
+     *
+     * 1 -> 1
+     * 2 -> 1\nFizz
+     * 3 -> 1\nFizz\nBuzz
+     * 6 -> 1\nFizz\nBuzz\nFizz\n5\nFizzBuzz
+     */
+
+}
