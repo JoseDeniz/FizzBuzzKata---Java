92a885d1340ac82e28411bf0a90f1fc4c76e8985
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2015-09-22 22:40:18 +0100

    Refactor: correct the real FizzBuzz kata

--- src/FizzBuzzParser.java
+++ src/FizzBuzzParser.java
@@ -10,20 +10,20 @@ public class FizzBuzzParser {
     public static String parse(int input) {
         return IntStream.rangeClosed(1, input)
                         .mapToObj(FizzBuzzParser::parseNumber)
-                        .collect(joining("\n"));
+                        .collect(joining(","));
     }
 
     private static String parseNumber(int number) {
-        String parsedNumber = parseIsDivisibleByTwo(number).concat(parseIsDivisibleByThree(number));
+        String parsedNumber = parseIsDivisibleByThree(number).concat(parseIsDivisibleByFive(number));
         return parsedNumber.equals(EMPTY) ? valueOf(number) : parsedNumber;
     }
 
-    private static String parseIsDivisibleByTwo(int number) {
-        return number % 2 == 0 ? "Fizz" : EMPTY;
+    private static String parseIsDivisibleByThree(int number) {
+        return number % 3 == 0 ? "Fizz" : EMPTY;
     }
 
-    private static String parseIsDivisibleByThree(int number) {
-        return number % 3 == 0 ? "Buzz" : EMPTY;
+    private static String parseIsDivisibleByFive(int number) {
+        return number % 5 == 0 ? "Buzz" : EMPTY;
     }
 
 }
--- tests/FizzBuzzParserShould.java
+++ tests/FizzBuzzParserShould.java
@@ -11,18 +11,18 @@ public class FizzBuzzParserShould {
     }
 
     @Test
-    public void parse_the_number_divisible_by_two_to_fizz() {
-        assertThat(FizzBuzzParser.parse(2), is("1\nFizz"));
+    public void parse_the_number_divisible_by_three_to_fizz() {
+        assertThat(FizzBuzzParser.parse(3), is("1,2,Fizz"));
     }
 
     @Test
-    public void parse_the_number_divisible_by_three_to_buzz() {
-        assertThat(FizzBuzzParser.parse(3), is("1\nFizz\nBuzz"));
+    public void parse_the_number_divisible_by_five_to_buzz() {
+        assertThat(FizzBuzzParser.parse(5), is("1,2,Fizz,4,Buzz"));
     }
 
     @Test
-    public void parse_the_number_divisible_by_two_and_three_to_fizzbuzz() {
-        assertThat(FizzBuzzParser.parse(6), is("1\nFizz\nBuzz\nFizz\n5\nFizzBuzz"));
+    public void parse_the_number_divisible_by_three_and_five_to_fizzbuzz() {
+        assertThat(FizzBuzzParser.parse(15), is("1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz"));
     }
 
 }
